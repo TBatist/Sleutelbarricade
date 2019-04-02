cf4e4a7326714a23437c60d9523cc5459639d7b5
Tim Batist <timbatist1999@hotmail.nl>
2019-04-02 13:26:39 +0200

    Keypressed in methode & uitgang in methode

--- src/Hoofdpersoon.java
+++ src/Hoofdpersoon.java
@@ -41,7 +41,7 @@ public class Hoofdpersoon extends JComponent {
 
     public static JComponent surrounding(char direction){
         JComponent[][] speelveld = GUI.getSpeelveld();
-        int[] temp = GUI.getHoofdpersoon();
+        int[] temp = GUI.getHoofdpersoonLocation();
         JComponent vakje = null;
         switch (direction){
             case 'r':
@@ -66,6 +66,54 @@ public class Hoofdpersoon extends JComponent {
                 break;
         }
         return vakje;
+    }
+
+    public static void moveHoofdpersoon(JComponent surrounding, char direction) {
+        int[] temp = GUI.getHoofdpersoonLocation();
+        Hoofdpersoon hoofdPersoon = GUI.getHoofdPersoon();
+        JComponent[][] speelveld = GUI.getSpeelveld();
+        switch (direction) {
+            case 'r':
+                if (temp[1] + 1 <= 9 && (surrounding instanceof Vakje) || (surrounding instanceof Obstakel && Hoofdpersoon.checkWaarde((Obstakel) surrounding)) || (surrounding instanceof Schaar) || surrounding instanceof Uitgang) {
+                    if (surrounding instanceof Schaar) {
+                        hoofdPersoon.addSchaar((Schaar) surrounding);
+                    }
+                    speelveld[temp[0]][temp[1] + 1] = hoofdPersoon;
+                    speelveld[temp[0]][temp[1]] = new Vakje();
+                }
+                break;
+            case 'l':
+                if (temp[1] + 1 >= 0 && (surrounding instanceof Vakje) || (surrounding instanceof Obstakel && Hoofdpersoon.checkWaarde((Obstakel) surrounding)) || (surrounding instanceof Schaar) || surrounding instanceof Uitgang) {
+                    if (surrounding instanceof Schaar) {
+                        hoofdPersoon.addSchaar((Schaar) surrounding);
+                    }
+                    speelveld[temp[0]][temp[1] - 1] = hoofdPersoon;
+                    speelveld[temp[0]][temp[1]] = new Vakje();
+                }
+                break;
+            case 'u':
+                if (temp[0] - 1 >= 0 && (surrounding instanceof Vakje) || (surrounding instanceof Obstakel && Hoofdpersoon.checkWaarde((Obstakel) surrounding)) || (surrounding instanceof Schaar) || surrounding instanceof Uitgang) {
+                    if (surrounding instanceof Schaar) {
+                        hoofdPersoon.addSchaar((Schaar) surrounding);
+                    }
+                    speelveld[temp[0] - 1][temp[1]] = hoofdPersoon;
+                    speelveld[temp[0]][temp[1]] = new Vakje();
+                }
+                break;
+            case 'd':
+                if (temp[0] + 1 <= 9 && (surrounding instanceof Vakje) || (surrounding instanceof Obstakel && Hoofdpersoon.checkWaarde((Obstakel) surrounding)) || (surrounding instanceof Schaar) || surrounding instanceof Uitgang) {
+                    if (surrounding instanceof Schaar) {
+                        hoofdPersoon.addSchaar((Schaar) surrounding);
+                    }
+                    speelveld[temp[0] + 1][temp[1]] = hoofdPersoon;
+                    speelveld[temp[0]][temp[1]] = new Vakje();
+
+                }
+                break;
+        }
+        if (surrounding instanceof Uitgang) {
+            Uitgang.restartLevel();
+        }
     }
 
     public void paintComponent(Graphics g) {
